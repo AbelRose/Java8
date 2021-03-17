2eb8bb78b379250c33f8a327db41644af7f07b9c
abelrose <15941999082@163.com>
2021-03-17 08:06:22 +0800

    方法引用

--- src/main/java/com/matrix/TestLambda.java
+++ src/main/java/com/matrix/TestLambda.java
@@ -3,11 +3,17 @@ package com.matrix;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.function.Consumer;
 
 /**
  * @Author: yihaosun
  * @Date: 2021/3/16 10:06 下午
  */
 public class TestLambda {
+    @Test
+    public void test1() {
+        Consumer<String> con = System.out::println;
+        con.accept("matrix");
+    }
     
 }
